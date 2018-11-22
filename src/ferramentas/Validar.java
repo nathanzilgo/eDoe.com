1c1d647577e321c811c4ef104317a7b4bf0c7925
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2018-11-22 15:54:27 -0300

    Documentei algumas classes.

--- src/ferramentas/Validar.java
+++ src/ferramentas/Validar.java
@@ -10,28 +10,27 @@ public class Validar {
 		return valor.trim().equals("");
 	}
 
-	public static void adicionaUsuario(String id, String nome, String email, String celular, String classe)
-			throws Exception {
+	public static void adicionaUsuario(String id, String nome, String email, String celular, String classe) {
 		if (isNull(nome) || isEmpty(nome))
-			throw new Exception("Entrada invalida: nome nao pode ser vazio ou nulo.");
+			throw new IllegalArgumentException("Entrada invalida: nome nao pode ser vazio ou nulo.");
 		if (isNull(email) || isEmpty(email))
-			throw new Exception("Entrada invalida: email nao pode ser vazio ou nulo.");
+			throw new IllegalArgumentException("Entrada invalida: email nao pode ser vazio ou nulo.");
 		if (isNull(celular) || isEmpty(celular))
-			throw new Exception("Entrada invalida: celular nao pode ser vazio ou nulo.");
+			throw new IllegalArgumentException("Entrada invalida: celular nao pode ser vazio ou nulo.");
 		if (isNull(classe) || isEmpty(classe))
-			throw new Exception("Entrada invalida: classe nao pode ser vazia ou nula.");
-		if (isNull(id) || isEmpty(id))
-			throw new Exception("Entrada invalida: id do usuario nao pode ser vazio ou nulo.");
-	}
-
-	public static void validaId(String id) throws IllegalArgumentException {
+			throw new IllegalArgumentException("Entrada invalida: classe nao pode ser vazia ou nula.");
 		if (isNull(id) || isEmpty(id))
 			throw new IllegalArgumentException("Entrada invalida: id do usuario nao pode ser vazio ou nulo.");
 	}
 
-	public static void validaNome(String nome) throws Exception {
+	public static void validaId(String id) {
+		if (isNull(id) || isEmpty(id))
+			throw new IllegalArgumentException("Entrada invalida: id do usuario nao pode ser vazio ou nulo.");
+	}
+
+	public static void validaNome(String nome) {
 		if (isNull(nome) || isEmpty(nome))
-			throw new Exception("Entrada invalida: nome nao pode ser vazio ou nulo.");
+			throw new IllegalArgumentException("Entrada invalida: nome nao pode ser vazio ou nulo.");
 	}
 
 	public static Boolean checaArgumento(String campo) {

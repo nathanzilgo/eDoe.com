f6e4e228400b3617328f00cb1ec7905dc900fbc4
Gyovanna Costa <deborah.gyovanna.costa@ccc.ufcg.edu.br>
2018-11-28 20:19:07 -0300

    Adicionado teste de unidade toString da classe Usuario

---
+++ testes/projeto_lp2/UsuarioTest.java
@@ -0,0 +1,118 @@
+package projeto_lp2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import internas.Doador;
+import internas.Receptor;
+import internas.Usuario;
+
+class UsuarioTest {
+	private Usuario novoUsuario;
+
+	@Test
+	void testUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTelefone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetClasse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetItens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetItens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetItemInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAtualizaUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		// Cadastrando doador com cpf no id
+		this.novoUsuario = new Doador("01234567899", "Raquel Lopes", "raquel@computacao.ufcg.edu.br", "(83) 9990-9999",
+				"pessoa_fisica");
+		// Retorna representacao textual do doador cadastrado com cpf no id
+		assertEquals("Raquel Lopes/012.345.678-99, raquel@computacao.ufcg.edu.br, (83) 9990-9999, status: doador",
+				this.novoUsuario.toString());
+
+		// Cadastrando doador com cnpj no id
+		this.novoUsuario = new Doador("01234567000189", "Lar da garota", "lardagarota@gmail.com", "(83) 98762-4554",
+				"ong");
+		// Retorna representacao textual do doador cadastrado com cnpj no id
+		assertEquals("Lar da garota/01.234.567/0001-89, lardagarota@gmail.com, (83) 98762-4554, status: doador",
+				this.novoUsuario.toString());
+
+		// Cadastrando novo receptor com cpf no id
+		this.novoUsuario = new Receptor("84473712044", "Murilo Luiz Brito", "muriloluizbrito-81@ipmmi.org.br",
+				"(31) 99776-7434", "PESSOA_FISICA");
+		// Retorna representacao textual do receptor cadastrado com cpf no id
+		assertEquals(
+				"Murilo Luiz Brito/844.737.120-44, muriloluizbrito-81@ipmmi.org.br, (31) 99776-7434, status: receptor",
+				this.novoUsuario.toString());
+
+		// Cadastrando novo receptor com cnpj no id
+		this.novoUsuario = new Receptor("91724529000103", "Pedro Henrique", "pedrohenriquedavimoura_@keffin.com.br",
+				"(27) 98460-3045", "ORGAO_PUBLICO_MUNICIPAL");
+		// Retorna representacao textual do receptor cadastrado com cnpj no id
+		assertEquals(
+				"Pedro Henrique/91.724.529/0001-03, pedrohenriquedavimoura_@keffin.com.br, (27) 98460-3045, status: receptor",
+				this.novoUsuario.toString());
+
+	}
+
+	@Test
+	void testAdicionaItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExisteItemInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExisteItemItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetItemItem() {
+		fail("Not yet implemented");
+	}
+
+}

9e969aad25750a68beb1df45058c154574d465d6
Gyovanna Costa <deborah.gyovanna.costa@ccc.ufcg.edu.br>
2018-11-28 20:56:20 -0300

    Adicionado testes de unidade de todos os metodos get da classe Usuario

--- testes/projeto_lp2/UsuarioTest.java
+++ testes/projeto_lp2/UsuarioTest.java
@@ -2,14 +2,17 @@ package projeto_lp2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import internas.Doador;
+import internas.Item;
 import internas.Receptor;
 import internas.Usuario;
 
 class UsuarioTest {
 	private Usuario novoUsuario;
+	private Item novoItem;
 
 	@Test
 	void testSetNome() {
@@ -77,18 +80,34 @@ class UsuarioTest {
 	}
 
 	@Test
-	void testSetClasse() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	void testGetNome() {
-		fail("Not yet implemented");
+		// Cadastrando doador
+		this.novoUsuario = new Doador("01234567899", "Raquel Lopes", "raquel@computacao.ufcg.edu.br", "(83) 9990-9999",
+				"pessoa_fisica");
+		// Retorna nome do doador
+		assertEquals("Raquel Lopes", this.novoUsuario.getNome());
+
+		// Cadastrando novo receptor
+		this.novoUsuario = new Receptor("91724529000103", "Pedro Henrique", "pedrohenriquedavimoura_@keffin.com.br",
+				"(27) 98460-3045", "ORGAO_PUBLICO_MUNICIPAL");
+		// Retorna nome do receptor
+		assertEquals("Pedro Henrique", this.novoUsuario.getNome());
 	}
 
 	@Test
 	void testGetItens() {
-		fail("Not yet implemented");
+		// Cadastrando doador
+		this.novoUsuario = new Doador("01234567899", "Raquel Lopes", "raquel@computacao.ufcg.edu.br", "(83) 9990-9999",
+				"pessoa_fisica");
+		// Adicionando itens a doador
+		this.novoItem = new Item("cadeira de rodas", 1, "manual, adulto");
+		this.novoUsuario.adicionaItem(novoItem);
+		this.novoItem = new Item("curso sobre cuidados com o bebê", 3, "maternidade, duracao 12h");
+		this.novoUsuario.adicionaItem(novoItem);
+		// Retorna os itens do doador
+		assertEquals(
+				"{1=1 - cadeira de rodas, tags: [manual,  adulto], quantidade: 1, 2=2 - curso sobre cuidados com o bebê, tags: [maternidade,  duracao 12h], quantidade: 3}",
+				this.novoUsuario.getItens().toString());
 	}
 
 	@Test
@@ -98,7 +117,15 @@ class UsuarioTest {
 
 	@Test
 	void testGetItemInt() {
-		fail("Not yet implemented");
+		// Cadastrando doador
+		this.novoUsuario = new Doador("01234567899", "Raquel Lopes", "raquel@computacao.ufcg.edu.br", "(83) 9990-9999",
+				"pessoa_fisica");
+		// Adicionando item a doador
+		this.novoItem = new Item("cadeira de rodas", 1, "manual, adulto");
+		this.novoUsuario.adicionaItem(novoItem);
+		// Retorna o item do doador
+		assertEquals("1 - cadeira de rodas, tags: [manual,  adulto], quantidade: 1",
+				this.novoUsuario.getItem(1).toString());
 	}
 
 	@Test
@@ -152,11 +179,6 @@ class UsuarioTest {
 
 	@Test
 	void testExisteItemItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetItemItem() {
 		fail("Not yet implemented");
 	}
 

6df5df6f21806ac265cdf564085899c9b2180962
Gyovanna Costa <deborah.gyovanna.costa@ccc.ufcg.edu.br>
2018-12-13 10:47:23 -0300

    Retirado erros de teste JUnit de Item

--- testes/projeto_lp2/ItemTest.java
+++ testes/projeto_lp2/ItemTest.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import internas.Item;
+import internas.Usuario;
 
 class ItemTest {
-	Item i1 = new Item(1, "macarrao bom", 5, "macarrao,almondegas");
-	Item i2 = new Item(2, "sapatos novos", 2, "adidas,novo");
-	Item i3 = new Item(3, "macarrao bom", 3, "macarrao,almondegas");
+	Usuario novoUsuario = new Usuario("1111111", "Raquel Lopes", "raquel_lopes@ccc.ufcg.edu.br", "3333 - 3333", "pessoa_fisica", false);
+	Item i1 = new Item(1, "macarrao bom", 5, "macarrao,almondegas", novoUsuario);
+	Item i2 = new Item(2, "sapatos novos", 2, "adidas,novo", novoUsuario);
+	Item i3 = new Item(3, "macarrao bom", 3, "macarrao,almondegas", novoUsuario);
 
 	@Test
 	void testHashCode() {
